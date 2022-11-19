5163e34fda35f2b814f715d912391e6a321ccbe8
luampelus <106559375+luampelus@users.noreply.github.com>
2022-11-19 16:30:26 -0300

    portfolio terminado

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,46 +1,11 @@
-import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+import { useForm, ValidationError } from "@formspree/react";
 
 export const Contact = () => {
-  const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Enviar');
-  const [status, setStatus] = useState({});
-
-  const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonText("Enviando...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
-    } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-    }
-  };
+  const [state, handleSubmit] = useForm("xdojjaol");
 
   return (
     <section className="contact" id="connect">
@@ -48,40 +13,66 @@ export const Contact = () => {
         <Row className="align-items-center">
           <Col>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Contactame</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-2 py-2">
-                      <input type="text" value={formDetails.firstName} placeholder="Nombre" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-2 py-2">
-                      <input type="text" value={formDetails.lasttName} placeholder="Apellido" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-2 py-2">
-                      <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-2 py-2">
-                      <input type="tel" value={formDetails.phone} placeholder="Teléfono" onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-2 py-2">
-                      <textarea rows="6" value={formDetails.message} placeholder="Mensaje" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit" ><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Contactame</h2>
+                  <form onSubmit={handleSubmit}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-2 py-2">
+                        <input id="nombre" type="text" name="nombre" placeholder="Nombre" />
+                        <ValidationError
+                          prefix="Nombre"
+                          field="nombre"
+                          errors={state.errors}
+                        />
                       </Col>
-                    }
-                  </Row>
-                </form>
-              </div>}
+                      <Col size={12} sm={6} className="px-2 py-2">
+                        <input id="apellido" type="text" name="apellido" placeholder="Apellido" />
+                        <ValidationError
+                          prefix="Apellido"
+                          field="apellido"
+                          errors={state.errors}
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-2 py-2">
+                        <input id="email" type="email" name="email" placeholder="Email" />
+                        <ValidationError
+                          prefix="Email"
+                          field="email"
+                          errors={state.errors}
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-2 py-2">
+                        <input id="telefono" type="text" name="telefono" placeholder="Teléfono" />
+                        <ValidationError
+                          prefix="Teléfono"
+                          field="telefono"
+                          errors={state.errors}
+                        />
+                      </Col>
+                      <Col size={12} className="px-2 py-2">
+                        <textarea rows="6" id="mensaje" name="mensaje" placeholder="Mensaje" />
+                        <ValidationError
+                          prefix="Mensaje"
+                          field="mensaje"
+                          errors={state.errors}
+                        />
+                        <button type="submit" disabled={state.submitting}>
+                          Enviar
+                        </button>
+                      </Col>
+                    </Row>
+                  </form>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
